5c07dc9d1f4c41d3033887984628e2747493e9e3
OleksiiShprinher <aleksey.shpringer18@gmail.com>
2020-04-16 23:55:34 +0300

    11try

--- telegram_bot.py
+++ telegram_bot.py
@@ -9,12 +9,17 @@ bot = telebot.TeleBot("1193399427:AAGtR_BlibKtIeUBYcnyuIVJU4RotXvRFrA")
 def send_echo(message):
 	place_to_check = message.text
 
-	observation = owm.weather_at_place(place_to_check)
-	w = observation.get_weather()
+	try:
+		observation = owm.weather_at_place(place_to_check)
+	except BaseException:
+		answer = "I don`t know this place!"
+		bot.send_message(message.chat.id, answer)
+	else:		
+		w = observation.get_weather()
 
-	answer = 'In ' + place_to_check + ' now is ' + w.get_detailed_status() + "\n"
-	answer += 'The temperature there is ' + str(w.get_temperature('celsius')['temp']) + "\n\n"
+		answer = 'In ' + place_to_check + ' now is ' + w.get_detailed_status() + "\n"
+		answer += 'The temperature is ' + str(w.get_temperature('celsius')['temp']) + ' there'"\n\n"
 
-	bot.send_message(message.chat.id, answer)
+		bot.send_message(message.chat.id, answer)
 
 bot.polling( none_stop = True)
